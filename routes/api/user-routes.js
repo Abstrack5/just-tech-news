ba9b0b36c8a48a58d5f830c422ffbfa0e57a2477
Abstrack <Armon_ah05@outlook.com>
2022-05-31 08:08:48 -0400

    worked on validating password, connecting user login to their hash password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,29 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where:  {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with that email'});
+            return;
+        }
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(404).json({ message: 'Incorrect password!'});
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
+
+});
+
 // put /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
