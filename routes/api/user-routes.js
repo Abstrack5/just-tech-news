45ae498887c56d63914208f4a561fac94f5b0b57
Abstrack <Armon_ah05@outlook.com>
2022-06-02 16:43:08 -0400

    added comments, included in user and post routes findAll and findOne, can get comments /api/comments, can post comments /api/comments, and you can delete comments /api/comments/(comment id)

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote } =  require('../../models');
+const { User, Post, Vote, Comment } =  require('../../models');
 
 // get /api/users
 router.get('/', (req, res) => {
@@ -32,7 +32,36 @@ router.get('/:id', (req, res) => {
         ],
         where: {
             id: req.params.id
-        }
+        },
+        include: [
+            {
+                model: Post,
+                attributes: [
+                'id',
+                'title',
+                'post_url',
+                'created_at'
+                ]
+            },
+            {
+                model: Comment,
+                attributes: [
+                'id',
+                'comment_text',
+                'created_at'
+                ],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
