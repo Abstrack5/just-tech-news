855a52a960ddbabb5278ace7bede660f9f79fe3b
Abstrack <Armon_ah05@outlook.com>
2022-06-07 21:00:35 -0400

    implemented login, logout, and cookies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +9,19 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
- 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
